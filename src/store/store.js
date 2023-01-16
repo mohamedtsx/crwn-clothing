6cf6b7bff651df539fa72d26d615f2c00e1432d6
Mohamed <ma0068664@gmail.com>
2023-01-16 10:25:46 +0200

    add redux-persisit

--- src/store/store.js
+++ src/store/store.js
@@ -1,26 +1,18 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './root.reducer';
+
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
+import logger from './middleware/logger.js';
 
-const logger = store => next => action => {
-    if (!action.type) {
-        return next(action)
-    }
 
-    const date = new Date();
-    const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}:${date.getMilliseconds()}`;
+const middleware = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
 
-    console.group('%c action: %c %s %c @ %s', 'color: gray', '', action.type, 'color: gray', time);
-    console.log('%c prev state: %o', 'color: gray', store.getState());
-    console.log('%c action: %o', 'color: blue', action);
-    next(action);
-    console.log('%c Next State: %o', 'color: green', store.getState());
-    console.groupEnd();
-}
 
-const enhancer = applyMiddleware(logger);
+const composeEnhancers = compose(applyMiddleware(...middleware));
+
+// const enhancer = applyMiddleware(logger);
 
 const persistConfig = {
     key: 'root',
@@ -30,5 +22,5 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = createStore(persistedReducer, undefined, enhancer);
+export const store = createStore(persistedReducer, undefined, composeEnhancers);
 export const persistor = persistStore(store);
