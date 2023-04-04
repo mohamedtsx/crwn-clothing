d1ee95bd9d1c576eef46b63e0e60b510ac2091a9
Mohamed Khaled <ma0068664@gmail.com>
2023-04-04 14:53:20 +0200

    fix dynamic spinner import

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { checkUserSession } from './store/user/user.actions';
 
+import Spinner from './components/spinner/spinner.component';
+
 const Home = lazy(() => import('./routes/home/home.component'));
 const Authentication = lazy(() => import('./routes/authentication/authentication.component'));
 const Navigation = lazy(() => import('./routes/navigation/navigation.component'));
 const Shop = lazy(() => import('./routes/shop/shop.component'));
 const Checkout = lazy(() => import('./routes/checkout/checkout.component'));
 const Category = lazy(() => import('./components/category/category.component'));
-const Spinner = lazy(() => import('./components/spinner/spinner.component'));
 
 
 
