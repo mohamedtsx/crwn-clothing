d9e7d4cea6d999eab8538dd7e55bb19eadcc4bf8
Mohamed Khaled <ma0068664@gmail.com>
2023-04-04 09:33:44 +0200

    add dynamic import feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import { useEffect, lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './routes/home/home.component';
-import Authentication from './routes/authentication/authentication.component';
-import Navigation from './routes/navigation/navigation.component';
-import Shop from './routes/shop/shop.component';
-import Checkout from './routes/checkout/checkout.component';
-import Category from './components/category/category.component';
 
-import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { checkUserSession } from './store/user/user.actions';
+
+const Home = lazy(() => import('./routes/home/home.component'));
+const Authentication = lazy(() => import('./routes/authentication/authentication.component'));
+const Navigation = lazy(() => import('./routes/navigation/navigation.component'));
+const Shop = lazy(() => import('./routes/shop/shop.component'));
+const Checkout = lazy(() => import('./routes/checkout/checkout.component'));
+const Category = lazy(() => import('./components/category/category.component'));
+const Spinner = lazy(() => import('./components/spinner/spinner.component'));
+
 
 
 const App = () => {
@@ -18,22 +21,19 @@ const App = () => {
     dispatch(checkUserSession())
   }, [dispatch])
 
-
   return (
-    <Routes>
-      <Route path='/' element={<Navigation/>}> 
-        <Route index element={<Home/>}/>
-        
-        <Route path='authentication' element={<Authentication/>}/>
-        
-        <Route path='shop/*' element={<Shop/>}>
-          <Route path=':category' element={<Category/>}/>
-        </Route> 
-        
-        <Route path='checkout' element={<Checkout/>}/>
-
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner/>}>
+      <Routes>
+        <Route path='/' element={<Navigation/>}> 
+          <Route index element={<Home/>}/>
+          <Route path='authentication' element={<Authentication/>}/>
+          <Route path='shop/*' element={<Shop/>}>
+            <Route path=':category' element={<Category/>}/>
+          </Route> 
+          <Route path='checkout' element={<Checkout/>}/>
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
